6ce34d8cf47c08d5b0aa3b54b3c66ef70ab35f30
Mattijn Spitteler <spittelermattijn@gmail.com>
2022-11-25 12:04:00 +0100

    Create subdirectories in audio/ if they aren't there yet

--- audio/create-opus.sh
+++ audio/create-opus.sh
@@ -13,6 +13,7 @@ for i in $wav_files; do
     opus_packets_file="${opus_file}_packets"
     pcm_file="${opus_file%.opus}.pcm"
 
+    mkdir -p "`dirname "$pcm_file"`"
     sox "$i" -t raw -r 48000 -b 16 -c 1 -L -e signed-integer "$pcm_file"
     ./opusenc "$pcm_file" "$opus_file" "$opus_packets_file"
     rm "$pcm_file"
